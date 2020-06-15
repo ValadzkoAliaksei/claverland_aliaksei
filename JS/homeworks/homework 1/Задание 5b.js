565ff307eea10bcf150ba5150487d7eeebc7adb0
Valadzkoaliaksei <Valadzkoaliaksei@tut.by>
2020-06-15 14:19:20 +0300

    Типы данных и операторы (домашка) - добавил выбор ширины каждого изображения

--- JS/homeworks/homework 1/Задание 5b.js
+++ JS/homeworks/homework 1/Задание 5b.js
@@ -2,35 +2,75 @@ let numberImg;
 let widthImg;
 let widthBody = 1024;
 let marginImg = 5;
-do {
-    alert('Пожалуста, введите численные значения. Кроме 0');
-    numberImg = +prompt("Сколько изображений в галерее?");
-    widthImg = +prompt("Какова ширина каждого изображения?");
-    if ((widthImg + marginImg * 2) > widthBody) {
-        alert('Ширина изображения больше ширины страницы. Пожалуста, введите ширину изображения меньше 1015');
-    }
-} while (!numberImg || !widthImg || (widthImg + marginImg * 2) > widthBody);
 let body = document.querySelector('.body');
 body.style.width = `${widthBody}px`;
-let numberImgRow = (widthBody - widthBody % (widthImg + marginImg * 2)) / (widthImg + marginImg * 2);
-if (numberImgRow < numberImg) {
-    alert(`количество изображений в каждом ряду ${numberImgRow}`);
-    let numberRow = (numberImg - numberImg % numberImgRow) / numberImgRow;
-    alert(`количество полных рядов в галерее ${numberRow}`);
-    let numberImgLastRow = numberImg - numberImgRow * numberRow;
-    alert(`количество изображений в последнем ряду ${numberImgLastRow}`);
-} else if (numberImg == 1) {
-    alert(`Получился 1 неполный ряд и в нем ${numberImg} изображениe`);
-    } else if (numberImg >= 5) {
-        alert(`Получился 1 неполный ряд и в нем ${numberImg} изображений`);
+if (confirm ("Все изображения одинаковой ширины?")) {
+    do {
+        alert("Пожалуста, введите численные значения. Кроме 0");
+        numberImg = +prompt("Сколько изображений в галерее?");
+        widthImg = +prompt("Какова ширина каждого изображения в пикселях?");
+        if ((widthImg + marginImg * 2) > widthBody) {
+            alert("Ширина изображения больше ширины страницы. Пожалуста, введите ширину изображения меньше 1015");
+        }
+    } while (!numberImg || !widthImg || (widthImg + marginImg * 2) > widthBody);
+    let numberImgRow = (widthBody - widthBody % (widthImg + marginImg * 2)) / (widthImg + marginImg * 2);
+    if (numberImgRow < numberImg) {
+        alert(`Количество изображений в каждом ряду ${numberImgRow}`);
+        let numberRow = (numberImg - numberImg % numberImgRow) / numberImgRow;
+        alert(`Количество полных рядов в галерее ${numberRow}`);
+        let numberImgLastRow = numberImg - numberImgRow * numberRow;
+        if (numberImgLastRow !=0) {
+            alert(`Количество изображений в последнем ряду ${numberImgLastRow}`);
         } else {
-            alert(`Получился 1 неполный ряд и в нем ${numberImg} изображения`);
+            alert(`Количество изображений в последнем ряду ${numberImgRow}`);
         };
-for (let i = 0; i < numberImg; i += 1) {
-    let img = document.createElement('img');
-    img.className = 'img';
-    img.src = 'wrw.png';
-    body.appendChild(img);
-    img.style.width = `${widthImg}px`;
-    img.style.margin = `${marginImg}px`;
+    } else if (numberImg == 1) {
+        alert(`Получился 1 неполный ряд и в нем ${numberImg} изображениe`);
+        } else if (numberImg >= 5) {
+            alert(`Получился 1 неполный ряд и в нем ${numberImg} изображений`);
+            } else {
+                alert(`Получился 1 неполный ряд и в нем ${numberImg} изображения`);
+            };
+    for (let i = 0; i < numberImg; i += 1) {
+        let img = document.createElement('img');
+        img.className = 'img';
+        img.src = 'wrw.png';
+        body.appendChild(img);
+        img.style.width = `${widthImg}px`;
+        img.style.margin = `${marginImg}px`;
+    };
+} else {
+    let sumWidthImg = 0;
+    let imgRow = 1;
+    let sumI = 0;
+    do {
+        alert("Пожалуста, введите численное значение. Кроме 0");
+        numberImg = +prompt("Сколько изображений в галерее?");
+    } while (!numberImg);
+    for (let i = 1; i <= numberImg; i += 1) {
+        do {
+            widthImg = +prompt(`Введите ширину ${i} изображения в пикселях числом меньше 1015`);
+        } while (!widthImg || (widthImg + marginImg * 2) > widthBody);
+        sumWidthImg = sumWidthImg + widthImg + marginImg * 2;
+        if (sumWidthImg <= widthBody) {
+            sumI++
+        } else {
+            alert(`Количество изображений в ${imgRow} ряду равно ${sumI}`);
+            imgRow++;
+            sumI = 1;
+            sumWidthImg = widthImg + marginImg * 2;
+        };
+        let img = document.createElement('img');
+        img.className = 'img';
+        img.src = 'wrw.png';
+        body.appendChild(img);
+        img.style.width = `${widthImg}px`;
+        img.style.margin = `${marginImg}px`;
+    };
+    if (imgRow == 1) {
+        alert(`Количество изображений в ${imgRow} не полном ряду равно ${sumI}`);
+    } else {
+        alert(`Количество рядов в галерее ${imgRow}`);
+        alert(`Количество изображений в последнем ${imgRow} ряду равно ${sumI}`);
+    };
 };
